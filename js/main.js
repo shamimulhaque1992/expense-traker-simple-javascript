7ae286c7e9f0af0eb0813da0ee1192998b79e644
shamimulhaque1992 <khandoker15-1992@diu.edu.bd>
2022-02-17 23:52:51 +0600

    eleventh commit: production updates update

--- js/main.js
+++ js/main.js
@@ -1,29 +1,31 @@
+//doing the income and expenses operation
+
 function incomeExpenses(income_id, food_id, rent_id, clothes_id, total_expenses_id, total_balance_id) {
     const income_input = parseFloat(document.getElementById(income_id + "-input").value);
-    // console.log(income_input);
+    
 
     const food_input = parseFloat(document.getElementById(food_id + "-input").value);
-    // console.log(food_input);
+    
 
     const rent_input = parseFloat(document.getElementById(rent_id + "-input").value);
-    // console.log(rent_input);
+    
 
     const clothes_input = parseFloat(document.getElementById(clothes_id + "-input").value);
-    // console.log(clothes_input);
+    
 
     const total_prev_expenses = parseFloat(document.getElementById(total_expenses_id).innerText)
-    // console.log(total_prev_expenses);
+    
 
     const total_prev_balance = parseFloat(document.getElementById(total_balance_id).innerText)
-    // console.log(total_prev_balance);
+    
 
     if (income_input % 1 != 0 || food_input % 1 != 0 || rent_input % 1 != 0 || clothes_input % 1 != 0) {
         document.getElementById("error_str").style.display = "block"
-        // console.log("it is a String");
+        
     }
     else if (income_input < 0 || food_input < 0 || rent_input < 0 || clothes_input < 0) {
         document.getElementById("error_neg").style.display = "block"
-        // console.log("it is a negative number");
+        
     }
     else {
         const updated_total_expenses = document.getElementById(total_expenses_id).innerText = food_input + rent_input + clothes_input;
@@ -43,6 +45,8 @@ function incomeExpenses(income_id, food_id, rent_id, clothes_id, total_expenses_
 
 }
 
+
+//doing the saving and remaining operation
 function savingandRemaining(income_id, saving_input_id, saving_id, balance_id, remaining_balance_id) {
     const income_input = parseFloat(document.getElementById(income_id + "-input").value);
     console.log(income_input);
@@ -50,12 +54,12 @@ function savingandRemaining(income_id, saving_input_id, saving_id, balance_id, r
     let saving_input = parseFloat(document.getElementById(saving_input_id).value);
     console.log(saving_input);
 
-    /* const saving =  parseFloat(document.getElementById(saving_id).innerText); */
+    
 
     const balance = parseFloat(document.getElementById(balance_id).innerText)
 
     
-    /* const updated_savings = document.getElementById(saving_id).innerText = income_input * (saving_input / 100); */
+    
 
     if (saving_input > income_input) {
         document.getElementById("error_inv_save").style.display = "block"
@@ -69,15 +73,15 @@ function savingandRemaining(income_id, saving_input_id, saving_id, balance_id, r
     else if (saving_input % 1 != 0) {
         document.getElementById("error_str_save").style.display = "block"
 
-        // console.log("it is a String");
+        
     }
     else if (saving_input < 0) {
         document.getElementById("error_neg_save").style.display = "block"
-        // console.log("it is a negative number");
+        
     }
     else {
         document.getElementById(saving_id).innerText = income_input * (saving_input / 100);
-        // document.getElementById("success_save").style.display = "block"
+        
     }
 
 
@@ -97,12 +101,12 @@ function savingandRemaining(income_id, saving_input_id, saving_id, balance_id, r
     else if (saving_input % 1 != 0) {
         document.getElementById("error_str_save").style.display = "block"
 
-        // console.log("it is a String");
+        
     }
     else if (saving_input < 0) {
-        // document.getElementById("error_neg_save").style.display = "block"
+        
         document.getElementById(remaining_balance_id).innerText = 0;
-        // console.log("it is a negative number");
+        
     }
     else {
         document.getElementById(remaining_balance_id).innerText = balance - updated_savings;
@@ -112,20 +116,16 @@ function savingandRemaining(income_id, saving_input_id, saving_id, balance_id, r
 
 }
 
+////doing the clear notification operation
 function clearNotifications(type) {
     document.getElementById(type).style.display = "none";
 }
-/* function clearInputField(income_id, food_id, rent_id, clothes_id) {
-    document.getElementById(income_id +"-input").value = "";
-    document.getElementById(food_id +"-input").value = "";
-    document.getElementById(rent_id +"-input").value = "";
-    document.getElementById(clothes_id +"-input").value = "";
-} */
 
 
+// button selection
 document.getElementById("calc-btn").addEventListener("click", function () {
     incomeExpenses("income", "food", "rent", "clothes", "total-expenses", "balance");
-    // clearInputField("income", "food", "rent", "clothes")
+    
 })
 
 
@@ -134,6 +134,8 @@ document.getElementById("save-btn").addEventListener("click", function () {
 })
 
 
+
+//error handiling and showing errors
 document.getElementById("error_str").addEventListener("click", function () {
     clearNotifications("error_str")
 })
